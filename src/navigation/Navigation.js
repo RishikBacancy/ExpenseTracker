2c00a694a5f2e34cbbb04c3f1589e4cae84b97f0
RishikBacancy <rishik.karanjiya@bacancy.com>
2022-05-03 10:42:13 +0530

    Setup Navigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -6,28 +6,43 @@ import ManageExpense from '../screens/ManageExpense';
 import RecentExpenses from '../screens/RecentExpenses';
 import AllExpenses from '../screens/AllExpenses';
 import { GlobalStyle } from '../constants/styles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
 	return (
-		<Tab.Navigator screenOptions={{
-            headerStyle: {
-                backgroundColor: GlobalStyle.colors.primary500
-            },
-            headerTintColor: "white",
-            tabBarStyle: {
-                backgroundColor: GlobalStyle.colors.primary500
-            },
-            tabBarActiveTintColor: GlobalStyle.colors.accent500,
-        }}>
-			<Tab.Screen name="Recent" component={RecentExpenses} options={{
-                title:"Recent Expense",
-                tabBarLabel: "Recent",
-                
-            }}/>
-			<Tab.Screen name="All" component={AllExpenses} />
+		<Tab.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: GlobalStyle.colors.primary500
+				},
+				headerTintColor: 'white',
+				tabBarStyle: {
+					backgroundColor: GlobalStyle.colors.primary500
+				},
+				tabBarActiveTintColor: GlobalStyle.colors.accent500
+			}}
+		>
+			<Tab.Screen
+				name="Recent"
+				component={RecentExpenses}
+				options={{
+					title: 'Recent Expense',
+					tabBarLabel: 'Recent',
+					tabBarIcon: ({ color, size }) => <Ionicons name="hourglass" size={size} color={color} />
+				}}
+			/>
+			<Tab.Screen
+				name="All"
+				component={AllExpenses}
+				options={{
+					title: 'Recent Expense',
+					tabBarLabel: 'Recent',
+					tabBarIcon: ({ color, size }) => <Ionicons name="calendar" size={size} color={color} />
+				}}
+			/>
 		</Tab.Navigator>
 	);
 };
